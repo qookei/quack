29742a9e413a42beee919be425a9b68bd018eb04
qookie <kacper.slominski72@gmail.com>
2019-02-03 11:55:00 +0100

    More minor mistake fixing

--- kernel/panic.c
+++ kernel/panic.c
@@ -5,7 +5,7 @@
 #include <mesg.h>
 
 void panic(const char *message, interrupt_cpu_state *state, int regs, int pf_error) {
-	
+
 	uint32_t cr3 = get_cr3();
 
 	set_cr3(def_cr3());
@@ -13,9 +13,8 @@ void panic(const char *message, interrupt_cpu_state *state, int regs, int pf_err
 	early_mesg(LEVEL_ERR, "kernel", "Kernel panic!");
 	early_mesg(LEVEL_ERR, "kernel", "Message: '%s'", message);
 	if (regs) {
-	
 		uint32_t fault_addr;
-   		asm volatile("mov %%cr2, %0" : "=r" (fault_addr));
+		asm volatile("mov %%cr2, %0" : "=r" (fault_addr));
 		if (pf_error) {
 			early_mesg(LEVEL_ERR, "kernel", "cr2: %08x", fault_addr);
 			early_mesg(LEVEL_ERR, "kernel", "cr3: %08x", cr3);
@@ -24,10 +23,10 @@ void panic(const char *message, interrupt_cpu_state *state, int regs, int pf_err
 		early_mesg(LEVEL_ERR, "kernel", "eip: %08x eflags: %08x esp: %08x edi: %08x esi: %08x", state->eip, state->eflags, state->esp, state->edi, state->esi);
 		early_mesg(LEVEL_ERR, "kernel", "cs: %04x ds: %04x", state->cs, state->ds);
 	}
-	
+
 	stack_trace(20);
 
-	early_panic(LEVEL_ERR, "kernel", "halting");
+	early_mesg(LEVEL_ERR, "kernel", "halting");
 
 	asm volatile ("1:\nhlt\njmp 1b");
 }
