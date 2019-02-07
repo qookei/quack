5cbba4cda88a009a608c9dde6f4c1cc855ed7b0d
qookie <kacper.slominski72@gmail.com>
2019-02-07 22:53:27 +0100

    Add a simple userspace system call lib

---
+++ initrd/src/sys/syscall.c
@@ -0,0 +1,90 @@
+#include "syscall.h"
+
+/* process */
+void sys_exit(int err_code) {
+	asm volatile ("int $0x30" : : "b"(err_code), "a"(0));
+}
+
+int32_t sys_getpid() {
+	int32_t pid;
+	asm volatile ("int $0x30" : "=b"(pid) : "a"(1));
+	return pid;
+}
+
+/* wait */
+int sys_waitpid(int32_t pid) {
+	int stat;
+	asm volatile ("int $0x30" : "=b"(stat) : "b"(pid), "a"(2));
+	return stat;
+}
+
+int sys_waitipc() {
+	asm volatile ("int $0x30" : : "a"(3));
+}
+
+/* sched */
+int32_t sys_spawn_new(int32_t parent, int is_privileged) {
+	int32_t child;
+	asm volatile ("int $0x30" : "=b"(child) : "a"(4), "b"(parent), "c"(is_privileged));
+	return child;
+}
+
+void sys_make_ready(int32_t pid, uintptr_t entry, uintptr_t stack) {
+	asm volatile ("int $0x30" : : "a"(5), "b"(pid), "c"(entry), "d"(stack));
+}
+
+/* memory */
+uintptr_t sys_get_phys_from(int32_t pid, uintptr_t virt) {
+	uintptr_t phys;
+	asm volatile ("int $0x30" : "=d"(phys) : "a"(6), "b"(pid), "c"(virt));
+	return phys;
+}
+
+void sys_map_to(int32_t pid, uintptr_t virt, uintptr_t phys) {
+	asm volatile ("int $0x30" : : "a"(7), "b"(pid), "c"(virt), "d"(phys));
+}
+
+void sys_unmap_from(int32_t pid, uintptr_t virt) {
+	asm volatile ("int $0x30" : : "a"(8), "b"(pid), "c"(virt));
+}
+
+uintptr_t sys_alloc_phys() {
+	uintptr_t phys;
+	asm volatile ("int $0x30" : "=b"(phys) : "a"(9));
+	return phys;
+}
+
+void *sys_sbrk(int increment) {
+	uintptr_t brk;
+	asm volatile ("int $0x30" : "=c"(brk) : "a"(10), "b"(increment));
+	return (void *)brk;
+}
+
+/* IPC */
+int sys_ipc_send(int32_t pid, size_t size, void *data) {
+	int stat;
+	asm volatile ("int $0x30" : "=c"(stat) : "a"(11), "b"(pid), "c"(size), "d"(data));
+	return stat;
+}
+
+int sys_ipc_recv(void *data) {
+	int stat;
+	asm volatile ("int $0x30" : "=c"(stat) : "a"(12), "d"(data));
+	return stat;
+}
+
+void sys_ipc_remove() {
+	asm volatile ("int $0x30" : : "a"(13));
+}
+
+int sys_ipc_queue_length() {
+	int len;
+	asm volatile ("int $0x30" : "=b"(len) : "a"(14));
+	return len;
+}
+
+int32_t sys_ipc_get_sender() {
+	int32_t sender;
+	asm volatile ("int $0x30" : "=b"(sender) : "a"(15));
+	return sender;
+}
---
+++ initrd/src/sys/syscall.h
@@ -0,0 +1,33 @@
+#ifndef SYSCALL_H
+#define SYSCALL_H
+
+#include <stdint.h>
+#include <stddef.h>
+
+/* process */
+void sys_exit(int err_code);
+int32_t sys_getpid();
+
+/* wait */
+int sys_waitpid(int32_t pid);
+int sys_waitipc();
+
+/* sched */
+int32_t sys_spawn_new(int32_t parent, int is_privileged);
+void sys_make_ready(int32_t pid, uintptr_t entry, uintptr_t stack);
+
+/* memory */
+uintptr_t sys_get_phys_from(int32_t pid, uintptr_t virt);
+void sys_map_to(int32_t pid, uintptr_t virt, uintptr_t phys);
+void sys_unmap_from(int32_t pid, uintptr_t virt);
+uintptr_t sys_alloc_phys();
+void *sys_sbrk(int increment);
+
+/* IPC */
+int sys_ipc_send(int32_t pid, size_t size, void *data);
+int sys_ipc_recv(void *data);
+void sys_ipc_remove();
+int sys_ipc_queue_length();
+int32_t sys_ipc_get_sender();
+
+#endif
