fd98baf091cecabaeea1d7be8a597c09ada2f736
qookie <kacper.slominski72@gmail.com>
2019-02-19 23:28:48 +0100

    Implement a runtime check to verify port 0xE9 actually exists when enabled

--- kernel/io/debug_port.c
+++ kernel/io/debug_port.c
@@ -1,5 +1,9 @@
 #include "debug_port.h"
 
+#ifndef NO_DEBUG_OUT
+static int debug_is_present = 0;
+#endif
+
 /*
  * Writing to port 0xE9 might (and probably will)
  * cause a freeze or a crash on real hardware.
@@ -8,7 +12,10 @@
  */
 void debug_write(uint8_t byte) {
 	#ifndef NO_DEBUG_OUT
-	outb(DEBUG_PORT, byte);
+	if (debug_is_present > 0)
+		outb(DEBUG_PORT, byte);
+	else if (!debug_is_present)
+		debug_is_present = inb(DEBUG_PORT) == 0xE9 ? 1 : -1;
 	#else
 	(void)byte;
 	#endif
