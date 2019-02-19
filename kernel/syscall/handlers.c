qookei/quack

commit 31600e86865305f113e0964266e8195e8ee8c564

Fix memory leak in ipc_send system call handler

kernel/syscall/handlers.c
@@ -151,6 +151,8 @@ void ipc_send_handler(uintptr_t *pid, uintptr_t *size, uintptr_t *data, void *un
 
 	if (!task_ipcsend(sched_get_task(*pid), sched_get_current(), *size, kdata))
 		*size = -3;
+
+	kfree(kdata);
 }
 
 void ipc_recv_handler(uintptr_t *unused1, uintptr_t *size, uintptr_t *data, void *unused2) {

commit 0c78975a029b568b6b5fb9a7054d5d3f56e6e483

Fix a bug introduced in a bugfix in commit 31600e86

kernel/syscall/handlers.c
@@ -130,10 +130,10 @@ void ipc_send_handler(uintptr_t *pid, uintptr_t *size, uintptr_t *data, void *un
 	void *kdata = kmalloc(*size);
 	copy_from_user(kdata, (void *)(*data), *size);
 
-	if (!task_ipcsend(sched_get_task(*pid), sched_get_current(), *size, kdata))
+	if (!task_ipcsend(sched_get_task(*pid), sched_get_current(), *size, kdata)) {
 		*size = -3;
-
-	kfree(kdata);
+		kfree(kdata);
+	}
 }
 
 void ipc_recv_handler(uintptr_t *unused1, uintptr_t *size, uintptr_t *data, void *unused2) {