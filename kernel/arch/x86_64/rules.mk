d6f9d9c14f1f8b4d2e8c08fadc0165bc59086414
qookie <kacper.slominski72@gmail.com>
2019-06-09 14:52:34 +0200

    kernel: move Makefile into the kernel source directory

--- kernel/arch/x86_64/rules.mk
+++ kernel/arch/x86_64/rules.mk
@@ -1,6 +1,6 @@
 CC = x86_64-elf-gcc
 ASM = nasm
 
-CFLAGS = -ffreestanding -O$(OLEVEL) -Wall -Wextra -std=gnu11 -Ikernel -Ikernel/lib -fno-omit-frame-pointer -mno-sse -mno-avx -nostdlib -DARCH=x86_64 -mcmodel=kernel -z max-page-size=0x1000 -DNO_DEBUG_OUT
+CFLAGS = -ffreestanding -O$(OLEVEL) -Wall -Wextra -std=gnu11 -I. -Ilib -fno-omit-frame-pointer -mno-sse -mno-avx -nostdlib -DARCH=x86_64 -mcmodel=kernel -z max-page-size=0x1000 -DNO_DEBUG_OUT
 ARCH_OBJ = x86_64.o
 LINKER_SCRIPT = linker.ld
