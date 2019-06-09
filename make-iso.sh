e9a04c74062fc8c3f3e916ff4e6790b6d0a6b4b3
qookie <kacper.slominski72@gmail.com>
2019-06-09 16:25:10 +0200

    kernel: various script fixes and improvements

--- make-iso.sh
+++ make-iso.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+make -C kernel $@
+
 mkdir -p isodir/boot/grub
 
 cat >isodir/boot/grub/grub.cfg << EOL
@@ -10,6 +12,6 @@ menuentry "quack - default" {
 }
 EOL
 
-cp quack.elf isodir/boot/quack.elf
+cp kernel/quack.elf isodir/boot/quack.elf
 grub-mkrescue -o quack.iso isodir > /dev/null
 rm -r isodir
--- utils/trace2funcs.py
+++ utils/trace2funcs.py
@@ -1,13 +1,19 @@
-import subprocess, fileinput, re
+import subprocess, fileinput, re, sys
 
 ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
+pattern = re.compile(r'[a-zA-Z0-9]*: #[0-9]+: (\[[0-9a-fA-F]{8,16}\])')
 
-for line in fileinput.input():
-    line = ansi_escape.sub('', line)
-    if not line.startswith("trace: #"):
-        print(line.strip('\n'))
-    else:
-        ready = line.split('[', 1)[-1][:8]
-        command = "addr2line -pfse kernel.elf " + ready
-        out = subprocess.check_output(command, shell=True)
-        print (line.strip('\n') + " -> " + out.decode("utf-8").strip())
+if len(sys.argv) < 2:
+        print("missing required argument: <filename>")
+        sys.exit(1)
+
+with fileinput.FileInput("-") as file_in:
+        for line in file_in:
+                line = ansi_escape.sub('', line).strip('\n')
+                match = pattern.search(line);
+                if not match:
+                        print(line)
+                else:
+                        address = match.group(1)[1:-1]
+                        out = subprocess.check_output("addr2line -pfse {} {}".format(sys.argv[1], address), shell=True)
+                        print(line + " -> " + out.decode("utf-8").strip())
