7929350d0ac635ee77c61ec0e8e4be25d57db5c1
qookie <kacper.slominski72@gmail.com>
2019-06-13 23:38:00 +0200

    make-iso: add option to generate a minimal image

--- make-iso.sh
+++ make-iso.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+if [[ $1 == "help" ]];
+then
+	echo "quack make-iso.sh"
+	echo "supported arguments:"
+	echo -e "\thelp - you are here"
+	echo -e "\ttiny - produces a minimal disk image(about 2x size reduction)"
+	exit 0
+fi
+
 mkdir -p isodir/boot/grub
 
 cat >isodir/boot/grub/grub.cfg << EOL
@@ -11,5 +20,18 @@ menuentry "quack - default" {
 EOL
 
 cp quack.elf isodir/boot/quack.elf
-grub-mkrescue -o quack.iso isodir > /dev/null
+
+compress=0
+if [[ $1 == "tiny" ]];
+then
+	compress=1
+fi
+
+if [[ $compress == 1 ]];
+then
+	grub-mkrescue --compress=xz --install-modules="part_msdos iso9660 multiboot configfile" -o quack.iso isodir > /dev/null
+else
+	grub-mkrescue -o quack.iso isodir > /dev/null
+fi
+
 rm -r isodir
