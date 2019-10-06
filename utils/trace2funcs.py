c461aeb2735d80dea0d5b319c7faeff2f9f75e05
Kacper Słomiński <kacper.slominski72@gmail.com>
2019-10-06 16:00:00 +0200

    utils: improve trace2funcs script
    The script now prints the address rip points to and strips the
    "(discriminator X)" strings from the output

--- utils/trace2funcs.py
+++ utils/trace2funcs.py
@@ -1,7 +1,12 @@
 import subprocess, fileinput, re, sys
 
 ansi_escape = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~]')
-pattern = re.compile(r'[a-zA-Z0-9]*: #[0-9]+: (\[[0-9a-fA-F]{8,16}\])')
+stack_pattern = re.compile(r'[a-zA-Z0-9]*: #[0-9]+: (\[[0-9a-fA-F]{8,16}\])')
+reg_pattern = re.compile(r'cpu: rip: ([0-9a-f]{8,16})')
+
+def addr2line(addr):
+    out = subprocess.check_output("addr2line -pfse {} {}".format(sys.argv[1], addr), shell=True)
+    return re.sub(r'(\(discriminator [0-9]*\))', '', out.decode("utf-8").strip())
 
 if len(sys.argv) < 2:
         print("missing required argument: <filename>")
@@ -10,10 +15,11 @@ if len(sys.argv) < 2:
 with fileinput.FileInput("-") as file_in:
         for line in file_in:
                 line = ansi_escape.sub('', line).strip('\n')
-                match = pattern.search(line);
-                if not match:
+                stack_match = stack_pattern.search(line);
+                reg_match = reg_pattern.search(line);
+                if not stack_match and not reg_match:
                         print(line)
+                elif stack_match:
+                        print(line + " -> " + addr2line(stack_match.group(1)[1:-1]))
                 else:
-                        address = match.group(1)[1:-1]
-                        out = subprocess.check_output("addr2line -pfse {} {}".format(sys.argv[1], address), shell=True)
-                        print(line + " -> " + out.decode("utf-8").strip())
+                        print(line + "\nrip -> " + addr2line(reg_match.group(1)))
