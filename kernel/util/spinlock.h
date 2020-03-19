0a0b7ad3c2c1704f160e376bc721812ecfad8ce7
Kacper Słomiński <kacper.slominski72@gmail.com>
2020-03-19 14:23:37 +0100

    kernel/util: make spinlock movable
    This implementation is possibly very bad(tm).

--- kernel/util/spinlock.h
+++ kernel/util/spinlock.h
@@ -4,10 +4,23 @@
 #include <atomic>
 
 struct spinlock {
+	friend void swap(spinlock &a, spinlock &b) {
+		a._locked = b._locked.exchange(a._locked);
+	}
+
 	spinlock() = default;
 	~spinlock() = default;
 	spinlock(const spinlock &) = delete;
 	spinlock &operator=(const spinlock &) = delete;
+
+	spinlock(spinlock &&other) {
+		swap(*this, other);
+	}
+
+	spinlock &operator=(spinlock &&other) {
+		swap(*this, other);
+		return *this;
+	}
 
 	void lock();
 	bool try_lock();
